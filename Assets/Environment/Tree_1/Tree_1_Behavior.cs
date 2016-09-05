5ab08dcb81854ff26667c4a6a833db0f04bf1757
amrnator <amrsaleh129@gmail.com>
2016-09-05 00:43:14 -0400

    Added skybox and made it pretty
    Cosmetic changes

---
+++ Assets/Environment/Tree_1/Tree_1_Behavior.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tree_1_Behavior : MonoBehaviour {
+
+    public Mesh Swap;
+
+    void OnCollisionEnter(Collision collision) {
+        Debug.Log("Tree collision");
+        gameObject.GetComponent<MeshFilter>().mesh = Swap;
+        //DestroyObject(gameObject);
+    }
+}
